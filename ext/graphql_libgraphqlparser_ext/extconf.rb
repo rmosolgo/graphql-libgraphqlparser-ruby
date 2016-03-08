8bd84f86a6d8a87b2f44ae73c2062bb377bba1aa
Dylan Thacker-Smith <Dylan.Smith@shopify.com>
2016-03-08 11:10:52 -0500

    Use libgraphqlparser if installed with a /usr prefix.

--- ext/graphql_libgraphqlparser_ext/extconf.rb
+++ ext/graphql_libgraphqlparser_ext/extconf.rb
@@ -1,6 +1,12 @@
 require 'mkmf'
 
-dir_config('graphql', '/usr/local/include/graphqlparser', '/usr/local/lib')
-abort 'missing libgraphqlparser' unless have_library 'graphqlparser'
+prefixes = %w(/usr /usr/local)
+if prefix = prefixes.find{ |prefix| Dir["#{prefix}/lib/libgraphqlparser*"].first }
+  dir_config('graphql', "#{prefix}/include/graphqlparser", "#{prefix}/lib")
+else
+  dir_config('graphql')
+end
+abort 'missing libgraphqlparser' unless have_library('graphqlparser')
+abort 'missing libgraphqlparser headers' unless have_header('c/GraphQLParser.h')
 
 create_makefile "graphql_libgraphqlparser_ext"
