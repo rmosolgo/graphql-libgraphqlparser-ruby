8cdeb3cd7c8241f517315cc103b547f3a26bfee3
Matt Bessey <mbessey@caring.com>
2018-06-12 12:42:59 +0100

    improve extension build errors for the next person

--- ext/graphql_libgraphqlparser_ext/extconf.rb
+++ ext/graphql_libgraphqlparser_ext/extconf.rb
@@ -7,7 +7,21 @@ else
   dir_config('graphqlparser')
 end
 
-abort 'missing libgraphqlparser' unless have_library('graphqlparser')
-abort 'missing libgraphqlparser headers' unless have_header('c/GraphQLParser.h')
+LIBRARY_MISSING_ERROR_MESSAGE = <<-EOT
+
+🚨 Could not find libgraphqlparser library. Have you installed it? 🚨
+  See: https://github.com/rmosolgo/graphql-libgraphqlparser-ruby#installation
+
+EOT
+
+HEADER_MISSING_ERROR_MESSAGE = <<-EOT
+
+🚨 Could not find libgraphqlparser headers. Is your library installed in a custom location? 🚨
+  See: https://github.com/rmosolgo/graphql-libgraphqlparser-ruby#overriding-the-location-of-libgraphqlparser
+
+EOT
+
+abort LIBRARY_MISSING_ERROR_MESSAGE unless have_library('graphqlparser')
+abort HEADER_MISSING_ERROR_MESSAGE unless have_header('c/GraphQLParser.h')
 
 create_makefile "graphql_libgraphqlparser_ext"
